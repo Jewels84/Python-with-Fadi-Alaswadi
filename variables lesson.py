f941a68b94cfa90e8222be4b97e0060f92cfae8b
JaWaHiR Mo <you@example.com>
2020-05-28 00:40:20 +0100

    4thcommit

--- variables lesson.py
+++ variables lesson.py
@@ -3,7 +3,9 @@ age= '12'
 salary = '1234.54'
 fname= 'Aysha'
 lname= 'Ali'
-print(lname.upper() + " " + fname)
-print(salary.replace('1234.54' , '987.65'))
+print(lname.upper() + " " + fname)   #calling the varible lname with its method UPPER
+
+print(salary.replace('1234.54' , '987.65'))  #replacying the value of the variable 
 print('Candidate full name=') 
 print(fname + "  " + lname)
+print('She is' + " " + age)
