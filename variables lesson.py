94c2e89f44fda81958c84ca9d779e6603ecbd31d
JaWaHiR Mo <you@example.com>
2020-05-28 00:35:26 +0100

    3rd commit

--- variables lesson.py
+++ variables lesson.py
@@ -3,7 +3,7 @@ age= '12'
 salary = '1234.54'
 fname= 'Aysha'
 lname= 'Ali'
-print(fname + " " + lname.upper())
+print(lname.upper() + " " + fname)
 print(salary.replace('1234.54' , '987.65'))
-print('Candidate full name=')
+print('Candidate full name=') 
 print(fname + "  " + lname)
