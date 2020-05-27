d806cce3957e2b14bf00d48b03608783f472fb8a
JaWaHiR Mo <you@example.com>
2020-05-27 23:26:25 +0100

    1st class Writing codes

--- new.py
+++ new.py
@@ -1,1 +1,11 @@
-print('Yes!')
+print('Yes!')                       #typing normaly
+print('A B C D E F G H I J')         
+print('\n')                         #adding a new line with(\n)
+print("K L M N O P Q \n")           #use (\n) inside the fun.
+print(''' R S T U V                 
+W
+
+X
+Y
+
+Z''')                                #adding multible lines by '''
