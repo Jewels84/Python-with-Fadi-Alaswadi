c77556fa178fa05bf07b9c560a54cf249a870af7
JaWaHiR Mo <you@example.com>
2020-05-28 11:40:31 +0100

    logical operators lesson

---
+++ operaters.py
@@ -0,0 +1,27 @@
+print(3>5)   #The lesson is reminder of the operators > ,< , ==, !=, >= , <=
+print(3<5)    # the output will be either true or false
+print(3==5)
+print(3==3)
+print(3 !=5)
+print(90 >= 10)
+print(10 >=40)
+#Now the logical operators AND/OR/NOT
+#1) AND
+# true and true = true
+#true and false = false
+#false and false = false
+#2) OR
+#true or true = true
+#True or false = False
+#false or False = False
+#3) NOT
+#(not true)= False
+#(not false)= true
+a,b = 3,6
+print(a==a and b==b)
+print(a>=b and b!=a)
+print(a<b or b!=a)
+print(not b>=a)
+
+#REMEMBER  to activate the hash to your codes:
+#highlight the lines press (ctrl + / )
