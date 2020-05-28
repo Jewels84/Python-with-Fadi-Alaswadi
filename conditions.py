998a2009d17a1719b795c81ae141a02a31bfee32
JaWaHiR Mo <you@example.com>
2020-05-28 23:53:37 +0100

    conditions

---
+++ conditions.py
@@ -0,0 +1,6 @@
+username = input('Student Number: ')
+Password = input('Password: ')
+if (username == 'jojo' or username== 'joly') and (Password== '123' or Password== '987'):
+    print(' WELCOME TO YOUR ACCOUNT') 
+else:
+    print('TRY AGAIN')
